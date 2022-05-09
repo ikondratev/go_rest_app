488bfe34b6f23cc2ed9df9d04181c6d9d2200120
kilya <ilya.kondratyev@kupibilet.ru>
2022-05-09 14:36:07 +0300

    [0003] Add simple server

--- main/app.go
+++ main/app.go
@@ -1,6 +1,36 @@
 package main
-import "fmt"
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Add configurable structure
+type App struct {
+	Port string
+}
+
+func (a *App) Start() {
+	addr := fmt.Sprintf(":%s", a.Port)
+	log.Printf("Starting app on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+// Load env
+func env(key, defaultValue string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return val
+}
+
+//  Main point
 func main() {
-	fmt.Printf("Hello world")
+	server := App{
+			Port: env("PORT", "7878"),
+	}
+	server.Start()
 }
