29777f3b502cd9cd13b653260f60b1d3ac035875
kilya <ilya.kondratyev@kupibilet.ru>
2024-01-30 20:02:44 +0200

    add bot

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_rest_app/main
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go_rest_app/main/clients/telegram"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Add configurable structure
+type App struct {
+	Port string
+}
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+// Start server with args
+func (a *App) Start() {
+	http.Handle("/ping", logreq(ping))
+	addr := fmt.Sprintf(":%s", a.Port)
+	log.Printf("Starting app on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+// Load env
+func env(key, defaultValue string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return val
+}
+
+// Add log for requrest as middleware
+func logreq(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case "GET":
+				http.Error(w, "404 not found.", http.StatusNotFound)
+				return
+			case "POST":
+				log.Printf("pathL %s", r.URL.Path)
+				f(w, r)
+			default:
+				fmt.Fprintf(w, "Sorry, only POST methods are supported.")	
+			}
+	})
+}
+
+// Ping page
+func ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong\n")
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token",
+		"default_token",
+		"access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
+
+//  Main point
+func main() {
+	server := App{
+			Port: env("PORT", "7878"),
+	}
+	telegram.New(tgBotHost, mustToken()).SendMessage(123, "message")
+	server.Start()
+}
